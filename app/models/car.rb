4f287c5072560dc76535da786d0443c4be5eddef
julien baret <julienco@gmail.com>
2021-12-15 15:50:27 +0100

    filters ok

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,13 @@
 class Car < ApplicationRecord
+  CATEGORY = ["Sportscar", "City Car", "Sedan", "All Natural", "Supercar", "SUV"]
+
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
   validates :name, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY,
+    message: "%{value} is not a valid category" }
   validates :city, presence: true
   validates :price_per_day, presence: true, numericality: { greater_than: 0 }
 end
